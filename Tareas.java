29abb957055e819449a495a2a8f631cd85b2c8cc
hector <heccormir2@alu.edu.gva.es>
2026-01-15 12:58:21 +0100

    Creado Tareas.java con dos métodos

---
+++ Tareas.java
@@ -0,0 +1,11 @@
+public class Tareas {
+    public void listarTareas(){
+        System.out.println("1.Tarea Aprendiendo GitHub\n" +
+                "2.Tarea Matemáticas\n" +
+                "3.Tarea Valenciano\n" +
+                "4.Tarea Filosofía");
+    }
+    public void añadirTarea(){
+        System.out.println("Se ha añadido la tarea correctamente");
+    }
+}
