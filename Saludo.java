dfe36231e29e57f61e5f6719b7dc444a11e842c6
hectorcorteell <heccormir2@alu.edu.gva.es>
2026-01-15 13:08:44 +0100

    Creado Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,8 @@
+public class Saludo {
+  public static void saludoInicial(){
+    System.out.println("Hola, bienvenid@");
+  }
+  public static void despedida(){
+    System.out.println("¡Hasta pronto!");
+  }
+}
